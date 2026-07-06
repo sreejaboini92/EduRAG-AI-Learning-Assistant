fc985a2c44340d2d764cc633b8c5b90937773c54
sreejaboini92 <sreejaboini92@gmail.com>
2026-07-06 21:09:41 +0530

    Implemented login page with validation and navigation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,66 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
 function Login() {
-  return <h1>Login Page</h1>;
-}
+  const [email, setEmail]=useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+    function handleLogin(event) {
+  event.preventDefault();
 
+  if (email === "") {
+    setError("Email is required");
+    return;
+  }
+
+  if (password === "") {
+    setError("Password is required");
+    return;
+  }
+
+  if (password.length < 6) {
+    setError("Password must be at least 6 characters");
+    return;
+  }
+
+  setError("");
+
+console.log("Login Successful");
+console.log("Email:", email);
+console.log("Password:", password);
+navigate("/dashboard");
+}
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Welcome Back!</h1>
+        <p>Login to continue learning with EduRAG.</p>
+        <form onSubmit={handleLogin}>
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          {error && <p className="error">{error}</p>}
+          <button type="submit">
+            Login
+          </button>
+        </form>
+        <p className="signup-text">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
+}
 export default Login;
